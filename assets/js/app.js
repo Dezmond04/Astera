3dd553839ad6d759f80fba08508c793e325e085b
Igor Karih <karich_igor@keepcode.ru>
2022-08-31 15:37:46 +0300

    added hover effect, added case popup, fix other bags

--- assets/js/app.js
+++ assets/js/app.js
@@ -14696,6 +14696,70 @@ var sliderSwiper = function sliderSwiper() {
       on: {}
     });
   }
+
+  if (document.querySelector(".popup-case-slider__swiper")) {
+    // Указываем скласс нужного слайдера
+    // Создаем слайдер
+    new core(".popup-case-slider__swiper", {
+      // Указываем скласс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      autoHeight: false,
+      speed: 500,
+      //touchRatio: 0,
+      //simulateTouch: false,
+      loop: true,
+      //preloadImages: false,
+      //lazy: true,
+      //   autoplay: {
+      //     delay: 3000,
+      //     disableOnInteraction: false,
+      //   },
+      // Скроллбар
+
+      /*
+                scrollbar: {
+                    el: '.swiper-scrollbar',
+                    draggable: true,
+                },
+                */
+      // Кнопки "влево/вправо"
+      navigation: {
+        prevEl: ".popup-case-slider__button.popup-case-slider__button--prev",
+        nextEl: ".popup-case-slider__button.popup-case-slider__button--next"
+      },
+      // Брейкпоинты
+
+      /*
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 0,
+                        autoHeight: true,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    992: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    1268: {
+                        slidesPerView: 4,
+                        spaceBetween: 30,
+                    },
+                },
+                */
+      // События
+      on: {}
+    });
+  }
 };
 
 /* harmony default export */ var sliders = (sliderSwiper);
